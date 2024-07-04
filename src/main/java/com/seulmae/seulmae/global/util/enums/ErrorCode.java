a5e37bebdc5ca4580ae715cb5908d4d934cf1b99
hany <hany0147@naver.com>
2024-07-04 16:21:12 +0900

    [UPGRADE] #12 RESPONSE & CODE

--- src/main/java/com/seulmae/seulmae/global/util/enums/ErrorCode.java
+++ src/main/java/com/seulmae/seulmae/global/util/enums/ErrorCode.java
@@ -66,13 +66,13 @@ public enum ErrorCode {
 
     NO_SUCH_ELEMENT(404, "G016", "NO_SUCH_ELEMENT"),
 
-    UNAUTHORIZED(401, "G022", "UNAUTHORIZED"),
-    MethodArgumentNotValidException(422, "G017", "MethodArgumentNotValidException"),
-    JwtTokenIsExpired(401,"G018" , "Jwt token is expired" ),
-    JwtTokenIsNotValidException(401,"G019" ,"Jwt token is not valid" ),
-    InvalidPasswordException(401,"G020" ,"Password does not match" ),
-    NoSuchUserException(401,"G021" ,"No such user exist" );
 
+    METHOD_ARGUMENT_NOT_VALID_EXCEPTION(422, "G017", "MethodArgumentNotValidException"),
+    JWT_TOKEN_IS_EXPIRED(401,"G018" , "Jwt token is expired" ),
+    JWT_TOKEN_IS_NOT_VALID_EXCEPTION(401,"G019" ,"Jwt token is not valid" ),
+    INVALID_PASSWORD_EXCEPTION(401,"G020" ,"Password does not match" ),
+    NO_SUCH_USER_EXCEPTION(401,"G021" ,"No such user exist" ),
+    UNAUTHORIZED(401, "G022", "UNAUTHORIZED");
 
     // 에러 코드의 '코드 상태'을 반환한다.
     private final int status;
--- src/main/java/com/seulmae/seulmae/global/util/enums/ErrorResponse.java
+++ src/main/java/com/seulmae/seulmae/global/util/enums/ErrorResponse.java
@@ -4,21 +4,18 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ErrorResponse {
     private int status;                 // 에러 상태 코드
-    private String divisionCode;        // 에러 구분 코드
-    private String resultMsg;           // 에러 메시지
-    private List<FieldError> errors;    // 상세 에러 메시지
-    private String reason;              // 에러 이유
+    private String customStatus;        // 에러 구분 코드
+    private String message;             // 에러 메시지
+    private String errorDescription;    // 에러 이유
+    private LocalDateTime timestamp;    // 에러 발생 시간
+    //    private List<FieldError> errors;    // 상세 에러 메시지
 
 
     /**
@@ -28,24 +25,26 @@ public class ErrorResponse {
      */
     @Builder
     protected ErrorResponse(final ErrorCode code) {
-        this.resultMsg = code.getMessage();
+        this.message = code.getMessage();
         this.status = code.getStatus();
-        this.divisionCode = code.getDivisionCode();
-        this.errors = new ArrayList<>();
+        this.customStatus = code.getDivisionCode();
+        this.timestamp = LocalDateTime.now();
+//        this.errors = new ArrayList<>();
     }
 
     /**
      * ErrorResponse 생성자-2
      *
      * @param code   ErrorCode
-     * @param reason String
+     * @param errorDescription String
      */
     @Builder
-    public ErrorResponse(final ErrorCode code, final String reason) {
-        this.resultMsg = code.getMessage();
+    public ErrorResponse(final ErrorCode code, final String errorDescription) {
+        this.message = code.getMessage();
         this.status = code.getStatus();
-        this.divisionCode = code.getDivisionCode();
-        this.reason = reason;
+        this.customStatus = code.getDivisionCode();
+        this.errorDescription = errorDescription;
+        this.timestamp = LocalDateTime.now();
     }
 
     /**
@@ -54,13 +53,13 @@ public class ErrorResponse {
      * @param code   ErrorCode
      * @param errors List<FieldError>
      */
-    @Builder
-    protected ErrorResponse(final ErrorCode code, final List<FieldError> errors) {
-        this.resultMsg = code.getMessage();
-        this.status = code.getStatus();
-        this.errors = errors;
-        this.divisionCode = code.getDivisionCode();
-    }
+//    @Builder
+//    protected ErrorResponse(final ErrorCode code, final List<FieldError> errors) {
+//        this.message = code.getMessage();
+//        this.status = code.getStatus();
+////        this.errors = errors;
+//        this.customStatus = code.getDivisionCode();
+//    }
 
 
     /**
@@ -70,9 +69,9 @@ public class ErrorResponse {
      * @param bindingResult BindingResult
      * @return ErrorResponse
      */
-    public static ErrorResponse of(final ErrorCode code, final BindingResult bindingResult) {
-        return new ErrorResponse(code, FieldError.of(bindingResult));
-    }
+//    public static ErrorResponse of(final ErrorCode code, final BindingResult bindingResult) {
+//        return new ErrorResponse(code, FieldError.of(bindingResult));
+//    }
 
     /**
      * Global Exception 전송 타입-2
@@ -99,33 +98,33 @@ public class ErrorResponse {
     /**
      * 에러를 e.getBindingResult() 형태로 전달 받는 경우 해당 내용을 상세 내용으로 변경하는 기능을 수행한다.
      */
-    @Getter
-    public static class FieldError {
-        private final String field;
-        private final String value;
-        private final String reason;
-
-        public static List<FieldError> of(final String field, final String value, final String reason) {
-            List<FieldError> fieldErrors = new ArrayList<>();
-            fieldErrors.add(new FieldError(field, value, reason));
-            return fieldErrors;
-        }
-
-        private static List<FieldError> of(final BindingResult bindingResult) {
-            final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
-            return fieldErrors.stream()
-                    .map(error -> new FieldError(
-                            error.getField(),
-                            error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
-                            error.getDefaultMessage()))
-                    .collect(Collectors.toList());
-        }
-
-        @Builder
-        FieldError(String field, String value, String reason) {
-            this.field = field;
-            this.value = value;
-            this.reason = reason;
-        }
-    }
+//    @Getter
+//    public static class FieldError {
+//        private final String field;
+//        private final String value;
+//        private final String reason;
+//
+//        public static List<FieldError> of(final String field, final String value, final String reason) {
+//            List<FieldError> fieldErrors = new ArrayList<>();
+//            fieldErrors.add(new FieldError(field, value, reason));
+//            return fieldErrors;
+//        }
+//
+//        private static List<FieldError> of(final BindingResult bindingResult) {
+//            final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
+//            return fieldErrors.stream()
+//                    .map(error -> new FieldError(
+//                            error.getField(),
+//                            error.getRejectedValue() == null ? "" : error.getRejectedValue().toString(),
+//                            error.getDefaultMessage()))
+//                    .collect(Collectors.toList());
+//        }
+//
+//        @Builder
+//        FieldError(String field, String value, String reason) {
+//            this.field = field;
+//            this.value = value;
+//            this.reason = reason;
+//        }
+//    }
 }
