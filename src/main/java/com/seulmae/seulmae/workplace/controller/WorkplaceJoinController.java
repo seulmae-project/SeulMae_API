41fbc25ab77062db155bfd9c00d730a2db269321
minyoung <myouth_u@naver.com>
2024-07-06 21:30:00 +0900

    FindByIdUtil update & Add Workplace Request List

--- src/main/java/com/seulmae/seulmae/workplace/controller/WorkplaceJoinController.java
+++ src/main/java/com/seulmae/seulmae/workplace/controller/WorkplaceJoinController.java
@@ -3,15 +3,15 @@ package com.seulmae.seulmae.workplace.controller;
 import com.seulmae.seulmae.global.util.enums.SuccessCode;
 import com.seulmae.seulmae.global.util.enums.SuccessResponse;
 import com.seulmae.seulmae.user.entity.User;
+import com.seulmae.seulmae.workplace.dto.WorkplaceJoinRequestDto;
 import com.seulmae.seulmae.workplace.service.WorkplaceJoinService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -35,11 +35,10 @@ public class WorkplaceJoinController {
     /**
      * 근무지 입장 수락
      * @param workplaceApproveId
-     * @param workplaceJoinHistoryId
      */
     @PostMapping("approval")
-    public ResponseEntity<?> sendJoinApproval(@RequestParam Long workplaceApproveId, @RequestParam Long workplaceJoinHistoryId) {
-        workplaceJoinService.sendJoinApproval(workplaceApproveId, workplaceJoinHistoryId);
+    public ResponseEntity<?> sendJoinApproval(@RequestParam Long workplaceApproveId) {
+        workplaceJoinService.sendJoinApproval(workplaceApproveId);
 
         SuccessResponse successResponse = new SuccessResponse(SuccessCode.INSERT_SUCCESS);
         return new ResponseEntity<>(successResponse, HttpStatus.CREATED);
@@ -48,13 +47,24 @@ public class WorkplaceJoinController {
     /**
      * 근무지 입장 거절
      * @param workplaceApproveId
-     * @param workplaceJoinHistoryId
      */
     @PostMapping("rejection")
-    public ResponseEntity<?> sendJoinRejection(@RequestParam Long workplaceApproveId, @RequestParam Long workplaceJoinHistoryId) {
-        workplaceJoinService.sendJoinRejection(workplaceApproveId, workplaceJoinHistoryId);
+    public ResponseEntity<?> sendJoinRejection(@RequestParam Long workplaceApproveId) {
+        workplaceJoinService.sendJoinRejection(workplaceApproveId);
 
         SuccessResponse successResponse = new SuccessResponse(SuccessCode.INSERT_SUCCESS);
         return new ResponseEntity<>(successResponse, HttpStatus.CREATED);
+    }
+
+    /**
+     * 근무지 입장 요청 리스트
+     * @param workplaceId
+     */
+    @GetMapping("request/list")
+    public ResponseEntity<?> getWorkplaceRequestList(@RequestParam Long workplaceId) {
+        List<WorkplaceJoinRequestDto> workplaceJoinRequestDtoList = workplaceJoinService.getWorkplaceRequestList(workplaceId);
+
+        SuccessResponse successResponse = new SuccessResponse(SuccessCode.SELECT_SUCCESS, workplaceJoinRequestDtoList);
+        return new ResponseEntity<>(successResponse, HttpStatus.OK);
     }
 }
--- src/main/java/com/seulmae/seulmae/workplace/repository/WorkplaceApproveRepository.java
+++ src/main/java/com/seulmae/seulmae/workplace/repository/WorkplaceApproveRepository.java
@@ -1,9 +1,18 @@
 package com.seulmae.seulmae.workplace.repository;
 
+import com.seulmae.seulmae.workplace.dto.WorkplaceJoinRequestDto;
 import com.seulmae.seulmae.workplace.entity.WorkplaceApprove;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface WorkplaceApproveRepository extends JpaRepository<WorkplaceApprove, Long> {
+
+    @Query("SELECT new com.seulmae.seulmae.workplace.dto.WorkplaceJoinRequestDto(wa.idWorkPlaceApprove, wa.user.name, wa.regDateWorkplaceApprove) " +
+            "FROM WorkplaceApprove wa " +
+            "WHERE wa.workplace.idWorkPlace = :workplaceId")
+    List<WorkplaceJoinRequestDto> findByWorkplaceId(Long workplaceId);
 }
