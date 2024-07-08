175df94fedc0f5f6985b7f6fba30bc53fc25dd4e
hany <hany0147@naver.com>
2024-07-08 15:41:49 +0900

    [ADD] #16 User Work Condition CRUD Test Code

--- src/main/java/com/seulmae/seulmae/user/dto/request/UserWorkScheduleAddRequest.java
+++ src/main/java/com/seulmae/seulmae/user/dto/request/UserWorkScheduleAddRequest.java
@@ -1,8 +1,12 @@
 package com.seulmae.seulmae.user.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserWorkScheduleAddRequest {
     private Long targetUserId;
     private Long workScheduleId;
