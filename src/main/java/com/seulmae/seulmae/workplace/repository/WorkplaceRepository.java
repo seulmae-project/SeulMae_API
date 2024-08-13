18adb4ed2f39f6bf715fdf0b9c45e87f03beb889
minyoung <myouth_u@naver.com>
2024-08-13 21:14:46 +0900

    Add keyword to the entire list of Workplace

--- src/main/java/com/seulmae/seulmae/workplace/repository/WorkplaceRepository.java
+++ src/main/java/com/seulmae/seulmae/workplace/repository/WorkplaceRepository.java
@@ -2,11 +2,17 @@ package com.seulmae.seulmae.workplace.repository;
 
 import com.seulmae.seulmae.workplace.entity.Workplace;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface WorkplaceRepository extends JpaRepository<Workplace, Long> {
 
+    @Query(value = "SELECT w " +
+            "FROM Workplace w " +
+            "WHERE (:keyword IS NULL OR w.workplaceName LIKE %:keyword%)")
+    List<Workplace> findAllByKeyword(String keyword);
 }
