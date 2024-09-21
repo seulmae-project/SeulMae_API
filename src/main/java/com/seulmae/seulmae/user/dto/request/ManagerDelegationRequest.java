9d9ce469c8a091cd0d68575f6a7db8a13da2ca00
minyoung <myouth_u@naver.com>
2024-09-21 16:54:18 +0900

    UserWorkplace Test Code

--- src/main/java/com/seulmae/seulmae/user/dto/request/ManagerDelegationRequest.java
+++ src/main/java/com/seulmae/seulmae/user/dto/request/ManagerDelegationRequest.java
@@ -1,8 +1,10 @@
 package com.seulmae.seulmae.user.dto.request;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class ManagerDelegationRequest {
     private Long userId;
     private Long workplaceId;
