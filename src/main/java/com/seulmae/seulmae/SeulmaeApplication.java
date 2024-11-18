22a51af336ac19fc218445499a759682b2bb5ce8
hany <hany0147@naver.com>
2024-11-18 21:49:43 +0900

    [ADD] ACTUATOR(MONITORING) SETTING(2)

--- src/main/java/com/seulmae/seulmae/SeulmaeApplication.java
+++ src/main/java/com/seulmae/seulmae/SeulmaeApplication.java
@@ -1,8 +1,10 @@
 package com.seulmae.seulmae;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -14,4 +16,8 @@ public class SeulmaeApplication {
 		SpringApplication.run(SeulmaeApplication.class, args);
 	}
 
+	@Bean
+	public InMemoryHttpExchangeRepository httpExchangeRepository() {
+		return new InMemoryHttpExchangeRepository();
+	}
 }
